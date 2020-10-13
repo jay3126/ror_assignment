999cbd64da30908b750042ce5358ad0bcf4a9a50
Janmejay Rai <janmejay.rai31@gmail.com>
2020-10-14 00:20:20 +0530

    fixing JS & CSS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 // require("packs")
 require("jquery")
+require("packs/select2")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference

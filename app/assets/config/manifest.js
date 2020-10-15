a22b5477cd492ce336ba2eb876aeb3d6886e9170
Janmejay Rai <janmejay.rai31@gmail.com>
2020-10-15 23:05:19 +0530

    some more time so 1 more commit

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../fonts
+//= link_tree ../fonts/font-awesome-4.7.0/fonts
+//= link_tree ../fonts/Linearicons-Free-v1.0.0/WebFont
+//= link_tree ../fonts/montserrat
+//= link_tree ../fonts/poppins
